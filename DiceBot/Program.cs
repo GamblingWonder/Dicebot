da3139a8e5d03c5354de12a05b6351fa8c058355
WinMachine777 <66855172+WinMachine777@users.noreply.github.com>
2022-09-18 11:40:31 -0700

    ...

---
+++ DiceBot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DiceBot
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                //Application.Run(new cLogin());
+                Application.Run(new cDiceBot(args));
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    using (StreamWriter sw = File.AppendText("DICEBOTLOG.txt"))
+                    {
+                        sw.WriteLine("########################################################################################\r\n\r\nFATAL CRASH\r\n");
+                        sw.WriteLine(e.ToString());
+                        sw.WriteLine("########################################################################################");
+                    }
+                }
+                catch { }
+                throw e;
+
+            }
+        }
+    }
+}
---
+++ DiceBot/StatsForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiceBot
+{
+    public partial class StatsForm : Form
+    {
+        public StatsForm()
+        {
+            InitializeComponent();
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            this.Controls.Remove(Controls[0]);
+            /*this.Hide();
+            e.Cancel = true;
+            base.OnClosing(e);*/
+        }
+        public void AddStatsWindow(StatsControl stats)
+        {
+            this.Controls.Add(stats);
+            stats.ShowHideButtons = true;
+        }
+    }
+}
